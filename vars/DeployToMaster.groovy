e7db6bd844f4997d01ae4ca6adb3680f87e3f04f
kguzmand <kguzmand@unal.edu.co>
2025-02-26 12:23:59 -0500

    Added Shared Library for dev branch

---
+++ vars/DeployToMaster.groovy
@@ -0,0 +1,5 @@
+// vars/DeployToMaster.groovy
+def call(Map parameters) {
+    echo "Deploying to master with param: ${parameters.anyparam}"
+    // Aquí puedes incluir las tareas de despliegue o cualquier lógica que quieras reutilizar
+}
